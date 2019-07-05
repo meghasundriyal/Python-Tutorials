a67a30feb7ac8ffafc8683a882e32a419632f73f
meghasundriyal15@gmail.com <meghasundriyal15@gmail.com>
2019-07-06 00:49:17 +0530

    string operations

---
+++ strings.py
@@ -0,0 +1,21 @@
+#performing different string operations
+a = "Sample! String  " #single line string
+b = '''This is an example 
+of multiline string '''                                       #double quotes can also be used
+print(a)
+print(b)
+
+#strings are treated as array of byte characters in python 
+print("a[1] : " ,a[1])
+print("a[2:7] : ",a[2:7])
+
+#METHODS ON STRINGS
+print("strip : ", a.strip())
+print("length of a : ",len(a))
+print("uppercase : ",a.upper())
+print("lowercase : ",a.lower())
+print("split : ",a.split("!"))
+
+print("Hello, world")
+print("Helllo","world")
+print("Hello"+"world")
