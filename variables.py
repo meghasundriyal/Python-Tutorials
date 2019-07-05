c715d0699f17d013429e7042b85513e4e0112003
meghasundriyal15@gmail.com <meghasundriyal15@gmail.com>
2019-07-05 23:59:36 +0530

    creating variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+#creating variables 
+string = "This is within quotes"           #string type 
+num = 5                   #number   
+comp_num = 1 + 8j         #complex number
+x = eval("1+2")           #evaluating  
+
+print("string : " , string)
+print("num : " ,num)
+print("comp_num : " ,comp_num)
+print("x : " ,x)
+
+#multiple assigments
+a,b,c = 1,2,"c"
+print(a,b,c)
+
+#assigning same value 
+x=y=55
+print(x,y)
