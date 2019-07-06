11969e6fdc55a187b0e2124b062e35aba77c49d6
meghasundriyal15@gmail.com <meghasundriyal15@gmail.com>
2019-07-06 12:09:07 +0530

    bitwise operators

---
+++ bitwise_operators.py
@@ -0,0 +1,13 @@
+#Bitwise opoerators
+x = y =5
+z = 2
+print("x = ", x,
+    "\ny = ", y,
+    "\nz = ", z)
+
+print("x & z : ", x & z)        #bitwise AND
+print("x | z : ", x | z)        #bitwise OR
+print("x ^ y : ", x ^ y)        #bitwise XOR
+print("x << z : ", x << z)       #left shift 
+print("x >> z : ", x >> z)       #right shift 
+print("~x : ", ~x)              # bitwise NOT
